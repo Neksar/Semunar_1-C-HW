e952053bbe2a23659b489b1e8ed8c65cd8cf2dc1
Maksim <m.bondarenko1988@yandex.ru>
2023-01-14 20:15:56 +0300

    Done first homework about 1st seminar by C#

--- homework_1/Program.cs
+++ homework_1/Program.cs
@@ -4,3 +4,19 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+Console.Write("Введите первое число: "); //Вводим первое число
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: "); // Вводим второе число
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2){ // Если первое число больше второго
+    Console.WriteLine($"Большее число: {number_1}");
+    System.Console.WriteLine($"Меньшее число: {number_2}");
+}
+else if (number_1 == number_2){ // Если числа равны
+    System.Console.WriteLine("Введенные числа между собой равны!");
+}
+else{ // Если второе число больше первого
+    System.Console.WriteLine($"Большее число: {number_2}");
+    System.Console.WriteLine($"Меньшее число: {number_1}");
+}
